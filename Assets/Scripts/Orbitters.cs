cbf4480c7f729fc670eb0b65fa515d9b989438d0
hatim ourahou <147079928+ourabigdev@users.noreply.github.com>
2026-05-08 12:59:54 +0100

    initial work

---
+++ Assets/Scripts/Orbitters.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
+
+public class Orbitters : MonoBehaviour
+{
+    public int sphereCount = 500;
+    public int maxRadius = 200;
+    public Material[] mats;
+    public Material trailMat;
+    
+    
+    private GameObject[] spheres;
+
+    void Awake()
+    {
+        spheres = new GameObject[sphereCount];
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spheres = CreateSphere(sphereCount, maxRadius);
+    }
+
+    public GameObject[] CreateSphere(int count, float radius)
+    {
+        var sphs = new GameObject[count];
+        var sphereToCopy = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        Rigidbody rb = sphereToCopy.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        for (int i = 0; i < count; i++)
+        {
+            var sp = GameObject.Instantiate(sphereToCopy);
+            sp.transform.position = this.transform.position +
+                                    new Vector3(Random.Range(-maxRadius, maxRadius),
+                                        Random.Range(-10, 10),
+                                        Random.Range(-maxRadius, maxRadius));
+            sp.transform.localScale *= Random.Range(0.5f, 1f);
+            sp.GetComponent<Renderer>().material = mats[Random.Range(0, mats.Length)];
+            TrailRenderer tr = sp.AddComponent<TrailRenderer>();
+            tr.time = 1f;
+            tr.startWidth = 0.1f;
+            tr.endWidth = 0f;
+            tr.material = trailMat;
+            tr.startColor = new Color(1,1,0,0.1f);
+            tr.endColor = new Color(0,0,0,0);
+            spheres[i] = sp;
+        }
+    
+        GameObject.Destroy(sphereToCopy);
+        
+        return spheres;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject s in spheres)
+        {
+            Vector3 difference = this.transform.position - s.transform.position;
+
+            float dist = difference.magnitude;
+            Vector3 gravityDirection = difference.normalized;
+            float gravity = 6.7f * (this.transform.localScale.x * s.transform.localScale.x * 80) / (dist * dist);
+
+            Vector3 gravityVector = (gravityDirection * gravity);
+            s.transform.GetComponent<Rigidbody>().AddForce(s.transform.forward, ForceMode.Acceleration);
+            s.transform.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
+        }
+    }
+}
